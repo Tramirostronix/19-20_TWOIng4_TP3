b9b6b47ef081fee484a0927f215c7f0c69c519f8
Tramirostronix <33159825+Tramirostronix@users.noreply.github.com>
2019-10-26 23:59:53 +0200

    fin_normalement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function start() {
 
     let ville=document.getElementById("city-input").value;
-  // Création de l'objet apiWeather
+  // Création de l'objet apiWeather avec la ville de l'utilisateur
   const apiWeather = new API_WEATHER(ville);
   // Appel de la fonction fetchTodayForecast
 
@@ -28,37 +28,32 @@ function start() {
       // Affiche une erreur
       console.error(error);
     });
-}
 
-function getThreeDayForecast() {
-    alert("3jours");
-    let ville=document.getElementById("city-input").value;
-    // Création de l'objet apiWeather
-    const apiWeather = new API_WEATHER(ville);
-    // Appel de la fonction fetchTodayForecast
 
+  //on recupere la meteo des 3 prochains jours
     apiWeather
         .fetchThreeDayForecast()
         .then(function(response) {
-            // Récupère la donnée d'une API
             const data = response.data;
+          let daysTwoMain = document.getElementsByClassName("days-two-forecast-main");
+          let daysTwoDescription = document.getElementsByClassName("days-two-forecast-more-info");
+          let daysTwoIcon = document.getElementsByClassName("days-two-icon-weather-container");
+          let daysTwoTemp = document.getElementsByClassName("days-two-forecast-temp");
+          let Temp;
+            //ici on doit "sauter" la premiere valeur car nous la possédons déjà pour récupérer les trois dernieres.
+            for(let i = 0; i<3; i++)
+            {
+                Temp = data.list[i + 1].temp.day;
+                daysTwoTemp[i].innerHTML = `${Temp}°C`;
+                daysTwoIcon[i].innerHTML = apiWeather.getHTMLElementFromIcon(data.list[i+1].weather[0].icon);
+                daysTwoDescription[i].innerHTML = data.list[i+1].weather[0].description;
+                daysTwoMain[i].innerHTML = data.list[i+1].weather[0].main;
 
-            // On récupère l'information principal
-            const main = data.weather[0].main;
-            const description = data.weather[0].description;
-            const temp = data.main.temp;
-            const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
 
-            // Modifier le DOM
-            document.getElementById('today-forecast-main').innerHTML = main;
-            document.getElementById('today-forecast-more-info').innerHTML = description;
-            document.getElementById('icon-weather-container').innerHTML = icon;
-            document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-
+            }
         })
         .catch(function(error) {
             // Affiche une erreur
             console.error(error);
         });
-
 }
